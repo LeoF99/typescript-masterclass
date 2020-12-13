3268e1e2b2af2f5b0a8aff5c611a9247931a2652
Leo Ferreira <leo.ferreira@dcx.ufpb.br>
2020-12-13 20:21:28 -0300

    Client implementation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
+import User from './components/User';
 
 function App() {
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    api.get<IUser[]>('/users').then(res => {
+      setUsers(res.data);
+    })
+  }, [])
+
   return (
     <div className="App">
-      
+      { users.map(user => <User key={user.email} user={user}/>)}
     </div>
   );
+}
+
+interface IUser {
+  name: string;
+  email: string;
 }
 
 export default App;
